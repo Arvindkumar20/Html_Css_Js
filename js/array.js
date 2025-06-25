56f1965f090fc5e64df636c2559b46ecc7e6ca30
arvindkumar20 <arvindkumar8960905167@gmail.com>
2025-06-25 12:01:33 +0530

    js folder added

---
+++ js/array.js
@@ -0,0 +1,77 @@
+const students = ["arvind", "suresh", "karthik", "suresh", "arvind", "karthik", "arvind"];
+
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+// students.push("Neeraj");
+// students.pop();
+
+// students.unshift("Neeraj");
+// students.shift();
+// let newStudents =students.slice(1, 3);
+
+// let Dublicat = students.filter((student) => student.includes("s"));
+// console.log(Dublicat);
+
+// let even = num.filter((number) => number % 2 === 0);
+// let odd = num.filter((number) => number % 2 !== 0);
+
+// console.log("even numbers = "+even);
+// console.log("odd numbers = "+odd);
+
+
+// let dublicate=students.map((student,index)=>student);
+// console.log(dublicate);
+
+// num.map((num)=>console.log(num*2));
+
+// students.map((student, index) => console.log(student.toUpperCase()));
+
+
+// num.map((number, index) => console.log(number*20));
+
+
+// for(let i=0;i<num.length;i++){
+//     console.log(num[i] * 10);
+// }
+
+// let i = 0;
+// while (i < num.length) {
+//     console.log(num[i] * 10);
+//     i++;
+// }
+
+// let i = 0;
+// do { console.log(num[i] * 5)
+//     i++;
+// } while (i < num.length);
+
+
+// num.forEach(number => {
+//     console.log(number * 10);
+// });
+
+// for (let number of num){
+//     console.log(number * 10);
+// }
+
+// for (let student in students) {
+//     console.log(student);
+// }
+
+
+
+
+
+
+
+
+
+// users.pop();
+// users.shift();
+// users.unshift("Added User");
+// let user = Math.random() * 6 + 1;
+// console.log(user)
+// user = Math.floor(user);
+
+
---
+++ js/index.js
@@ -0,0 +1,19 @@
+let product = "Electric"
+let price = 1000;
+let productName = "Electric Car";
+let productDescription = "An electric car with advanced features and eco-friendly technology.";
+let discount = 0.2;
+
+// console.log((price * discount) / 100);
+let total = price - (price * 0.2) / 100
+// console.log(total);
+
+
+let a = 20 % 3;
+// console.log("Remainder = "+a);
+
+let name = "RajVeer Kumar";
+
+console.log("Hello " + name + ", You have purchased " + productName + " for Rs." + total + ".\n" +    "Product Description: " + productDescription + "\n" +
+    "Thank you for shopping with us!");
+
